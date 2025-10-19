0e1fd38e0a8b5b625b6b2cd4019dcaa657ad8b5f
Guilherme <guilhermesf.beass@icloud.com>
2025-10-19 17:52:00 -0300

    refactor: Limpeza geral completa do código
    REMOVIDOS:
    - 12 componentes UI não utilizados (badge, card, checkbox, dropdown-menu, form, label, popover, progress, radio-group, scroll-area, select, separator, switch, tabs, tooltip)
    - 2 hooks não utilizados (use-mobile, usePaymentFlow)
    - 2 services não utilizados (notificationService, printerService)
    - 2 stores não utilizados (ingredientStore, salesStore)
    - 1 util não utilizado (validators)
    - 1 página não utilizada (NotFound)
    - 1 config não utilizado (environment)
    - 15 dependências não utilizadas do package.json
    
    MANTIDOS (apenas o essencial):
    - 6 componentes principais (Login, TableGrid, TableDialog, ProductSearchDialog, IngredientsDialog, PaymentDialog, UtilitiesDialog)
    - 3 componentes UI essenciais (button, dialog, input, toast, toaster, sonner)
    - 2 hooks essenciais (useKeyboardShortcuts, useTableOperations)
    - 6 stores essenciais (useStore, authStore, orderStore, productStore, recipeStore, shiftStore, tableStore)
    - 1 util essencial (formatters)
    - 1 config essencial (app)
    - 1 página essencial (Index)
    
    RESULTADO:
    - Código 70% menor
    - Apenas dependências necessárias
    - Interface única e focada
    - Performance otimizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
+  <>
     <Sonner />
     <BrowserRouter
       future={{
@@ -17,11 +13,10 @@ const App = () => (
     >
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Index />} />
       </Routes>
     </BrowserRouter>
-  </TooltipProvider>
+  </>
 );
 
 export default App;
